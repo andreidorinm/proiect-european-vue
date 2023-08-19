e39c22c155ca07878928f3b8acd4682b018b6ea4
andreidorinm <andrei.dorin09@yahoo.com>
2023-08-19 23:26:56 +0300

    added changes

---
+++ src/components/YearCarousel.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex items-center bg-white p-4 rounded-lg shadow-md">
+    <button 
+      @click="decrementYear"
+      class="px-3 py-2 text-white bg-red-500 rounded-lg shadow-md focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50"
+    >
+      ←
+    </button>
+    <span class="mx-4 text-lg font-medium">{{ currentYear }}</span>
+    <button 
+      @click="incrementYear"
+      class="px-3 py-2 text-white bg-green-500 rounded-lg shadow-md focus:outline-none focus:ring focus:ring-green-200 focus:ring-opacity-50"
+    >
+      →
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear()
+    };
+  },
+  methods: {
+    incrementYear() {
+      this.currentYear++;
+      this.$emit('yearChanged', this.currentYear);
+    },
+    decrementYear() {
+      this.currentYear--;
+      this.$emit('yearChanged', this.currentYear);
+    }
+  }
+}
+</script>
