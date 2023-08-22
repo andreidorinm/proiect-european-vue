b84029966b83cff330b8332d5979cfbb66a6ee35
andreidorinm <andrei.dorin09@yahoo.com>
2023-08-22 15:39:28 +0300

    multiple fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-<div class="mb-6">
-  <SecondaryNavbar/>
-</div>
-<HeroSection/>
+  <div class="mb-6">
+    <SecondaryNavbar />
+  </div>
+  <HeroSection />
 
   <main class="mt-4 bg-gray-50 dark:bg-gray-900">
-      <div class="px-4 py-6 sm:px-0">
-          <div class="p-4 text-center text-gray-400 border-4 border-gray-200 border-dashed rounded-lg h-full">
-              <GoogleMapComponent />
-          </div>
+    <div class="px-4 py-6 sm:px-0">
+      <div class="p-4 text-center text-gray-400 border-4 border-gray-200 border-dashed rounded-lg h-full">
+        <GoogleMapComponent />
       </div>
+    </div>
   </main>
 </template>
 
@@ -18,14 +18,25 @@
 import GoogleMapComponent from "../components/GoogleMapComponent.vue";
 import SecondaryNavbar from "../components/SecondaryNavbar.vue";
 import HeroSection from "../components/HeroSection.vue";
-import { defineComponent } from "vue";
+import { defineComponent, ref, provide } from "vue";
 
 
 export default defineComponent({
   components: {
-    GoogleMapComponent, 
+    GoogleMapComponent,
     SecondaryNavbar,
     HeroSection
+  },
+  setup() {
+    const selectedYear = ref(2010);
+    const updateSelectedYear = (year) => {
+      selectedYear.value = year;
+    }
+
+    provide('selectedYear', selectedYear);
+    provide('updateSelectedYear', updateSelectedYear);
+
+    return { selectedYear, updateSelectedYear };
   },
   data() {
     return {
--- src/views/MediaPage.vue
+++ src/views/MediaPage.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
+    <SecondaryNavbar/>
     <div v-if="yearEvents">
-      <h2>{{ currentYear }}</h2>
       <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div v-for="event in yearEvents" :key="event.title" class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+        <div v-for="event in yearEvents" :key="event.title"
+          class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
           <div class="h-52 flex flex-wrap overflow-hidden rounded-t-xl">
             <!-- Loop through the images for each event -->
-            <img v-for="image in event.images" :key="image.src" :src="image.src" :alt="image.alt" class="object-cover w-full h-full">
+            <img v-for="image in event.images" :key="image.src" :src="image.src" :alt="image.alt"
+              class="object-cover w-full h-full">
           </div>
           <div class="p-4 md:p-6">
             <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
@@ -19,7 +21,8 @@
               {{ event.description }}
             </p>
           </div>
-          <div class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
+          <div
+            class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
             <!-- The rest of your code remains unchanged -->
           </div>
         </div>
@@ -32,27 +35,65 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref, computed, onMounted, provide } from 'vue';
 import NotFound from './NotFound.vue';
-import imagesData from '/src/data/images.json';
+import SecondaryNavbar from '../components/SecondaryNavbar.vue';
 
 export default defineComponent({
+  name: 'MediaPage',
+
   components: {
-    NotFound
+    NotFound,
+    SecondaryNavbar
   },
+
   props: {
     year: {
       type: Number,
       required: true
     }
   },
-  computed: {
-    currentYear() {
-      return this.year;
-    },
-    yearEvents() {
-      return imagesData.find(data => data.year === this.currentYear)?.data;
+
+  setup(props, { emit }) {
+    const imagesData = ref([]);
+    const currentYear = ref(props.year);
+    provide('selectedYear', currentYear);
+
+    const yearEvents = computed(() => {
+      return imagesData.value.find(data => data.year === currentYear.value)?.data;
+    });
+
+    function updateSelectedYear(year) {
+      currentYear.value = year;
+      emit('year-updated', year);
     }
+
+    async function fetchYearData() {
+      try {
+        const response = await fetch('/data/images.json');
+        if (response.ok) {
+          imagesData.value = await response.json();
+        } else {
+          console.error('Failed to fetch the JSON data');
+        }
+      } catch (error) {
+        console.error('Error fetching JSON data:', error);
+      }
+    }
+
+    onMounted(() => {
+      fetchYearData();
+    });
+
+    // Provide the updateSelectedYear function for child components
+    provide('updateSelectedYear', updateSelectedYear);
+
+    return {
+      currentYear,
+      yearEvents
+    };
   }
-})
+});
+
 </script>
+
