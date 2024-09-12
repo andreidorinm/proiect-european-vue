049385c8d7f665810319f679fd014397eded3b82
andreidorinm <andrei.dorin09@yahoo.com>
2024-09-13 00:46:51 +0300

    added hypelink

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -5,10 +5,13 @@
     <div class="grid lg:grid-cols-7 lg:gap-x-8 xl:gap-x-12 lg:items-center">
       <div class="lg:col-span-3">
         <h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl lg:text-6xl dark:text-white">
-          Connecting we Grow<br> 
+          Connecting we Grow<br>
           2021-2026
         </h1>
-        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Prin Erasmus+, oferim școlii noastre o educație de top la standarde europene. Împreună inovăm, ne dezvoltăm și atingem excelența.</p>
+        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Prin Erasmus+, oferim <a
+            href="http://scoalaprofesionalasfnicolae.ro/" class="text-indigo-600 underline hover:text-indigo-500">școlii
+            noastre</a> o educație de top la standarde europene. Împreună inovăm, ne dezvoltăm și atingem
+          excelența.</p>
 
         <div class="mt-5 lg:mt-8 flex flex-col items-center gap-2 sm:flex-row sm:gap-3">
           <a class="w-full sm:w-auto inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800"
@@ -41,7 +44,7 @@
   </div>
   <!-- End Hero -->
 </template>
-  
+
 <script>
 import { defineComponent } from "vue";
 import GoogleMapComponent from "../components/GoogleMapComponent.vue";
@@ -56,4 +59,3 @@ export default defineComponent({
 
 )
 </script>
-  
